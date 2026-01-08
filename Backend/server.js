4a5ad8967bcd9d88d4aa0e5417082227bf1e461e
vinuvi-thalagala <vinuvithalagala01@outlook.com>
2026-01-08 11:21:55 +0530

    create server.js

---
+++ Backend/server.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+
+//import routes
+
+import driverRoutes from './routes/driver.routes.js'
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+dotenv.config();
+
+const PORT = process.env.PORT || 8070;
+
+const URL = process.env.MONGODB_URL;
+mongoose.connect(URL);
+
+const connection = mongoose.connection;
+
+connection.once('open', () => {
+    console.log('Mongodb connection is successfull');
+});
+
+app.use('/driver', driverRoutes);
+
+app.listen(PORT, () => {
+    console.log( `App is running on ${PORT}`);
+});
